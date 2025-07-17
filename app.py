a12291cd2c17d6b1c9b82ee1f3832b77a3859961
RIVI44 <riviamaharani0@gmail.com>
2025-07-17 15:00:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+gugus_fungsi_kamus = {
+    'COOH': 'Asam Karboksilat',
+    'CHO': 'Aldehid',
+    'CO': 'Keton',
+    'OH': 'Alkohol',
+    'NH2': 'Amina',
+    'COO': 'Ester',
+    'C=C': 'Alkena',
+    'C≡C': 'Alkuna'
+}
 
+def identifikasi_gugus_fungsi(rumus):
+    hasil = []
+    for gugus, nama in gugus_fungsi_kamus.items():
+        if gugus in rumus:
+            hasil.append(nama)
+    return hasil if hasil else ['Tidak teridentifikasi']
+
+rumus = input("Masukkan rumus senyawa: ")
+hasil_gugus = identifikasi_gugus_fungsi(rumus)
+print("Gugus fungsi teridentifikasi:", ", ".join(hasil_gugus))
