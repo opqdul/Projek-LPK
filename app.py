5f5c28ae8f20653efc1f3e1cf7fc56f7a3e362d2
Ridwan Nugroho <rnugraha305@gmail.com>
2025-07-19 14:23:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -141,12 +141,12 @@ def identifikasi():
         if gambar:
             st.image(f"https://raw.githubusercontent.com/RIVI44/LPK-KEDUA-/main/{gambar}", width=250)
             
-        st.write(f":white-background[*Rumus Diberikan:* {input_rumus}]")
-        st.write(f":white-background[*Rumus Distandarisasi:* {rumus}]")
-        st.write(f":white-background[*Gugus Fungsi Terdeteksi:* {', '.join(hasil)}]")
-        st.write(f":white-background[*Jenis Hidrokarbon:* {ikatan}]")
-        st.write(f":white-background[*Nama IUPAC:* {nama_iupac}]")
-        st.write(f":white-background[*Nama Trivial:* {nama_trivial}]")
+        st.markdown(f":white-background[*Rumus Diberikan:* {input_rumus}]")
+        st.markdown(f":white-background[*Rumus Distandarisasi:* {rumus}]")
+        st.markdown(f":white-background[*Gugus Fungsi Terdeteksi:* {', '.join(hasil)}]")
+        st.markdown(f":white-background[*Jenis Hidrokarbon:* {ikatan}]")
+        st.markdown(f":white-background[*Nama IUPAC:* {nama_iupac}]")
+        st.markdown(f":white-background[*Nama Trivial:* {nama_trivial}]")
 
 
 option = st.sidebar.radio(
