ca831fac8979d433fc785c62e182f55b2893988d
Ridwan Nugroho <rnugraha305@gmail.com>
2025-07-19 11:09:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -128,6 +128,7 @@ def identifikasi():
         st.write(f"*Rumus Diberikan:* {input_rumus}")
         st.write(f"*Rumus Distandarisasi:* {rumus}")
         st.write(f"*Gugus Fungsi Terdeteksi:* {', '.join(hasil)}")
+        st.write(f"*Jenis Hidrokarbon:* {hasil}")
         st.write(f"*Nama IUPAC:* {nama_iupac}")
         st.write(f"*Nama Trivial:* {nama_trivial}")
 
