7c7121d6ea956205bc640aa1963067927d00fe7e
Ridwan Nugroho <rnugraha305@gmail.com>
2025-07-19 13:59:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -138,10 +138,12 @@ def identifikasi():
 
         # Output
         st.markdown("### 🔍 Hasil Identifikasi")
-        st.write(f"*Rumus Diberikan:* {input_rumus}")
-        st.write(f"*Rumus Distandarisasi:* {rumus}")
+        
         if gambar:
             st.image(f"https://raw.githubusercontent.com/RIVI44/LPK-KEDUA-/main/{gambar}", width=250)
+            
+        st.write(f"*Rumus Diberikan:* {input_rumus}")
+        st.write(f"*Rumus Distandarisasi:* {rumus}")
         st.write(f"*Gugus Fungsi Terdeteksi:* {', '.join(hasil)}")
         st.write(f"*Jenis Hidrokarbon:* {ikatan}")
         st.write(f"*Nama IUPAC:* {nama_iupac}")
