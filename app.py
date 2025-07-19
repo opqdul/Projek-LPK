6ab0c0da3608123fac90d072c4b8ce805e1e248d
RIVI44 <riviamaharani0@gmail.com>
2025-07-19 17:59:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -101,18 +101,18 @@ kamus_nama_senyawa = {
 def tentang():
     st.title("Isi tentang")
     st.markdown("""
-    Aplikasi web ini dibuat dengan tujuan utama untuk *memudahkan pelajar dan mahasiswa dalam memahami serta mengklasifikasikan gugus fungsi dalam senyawa organik*. 
+    Aplikasi web ini dibuat dengan tujuan utama untuk memudahkan pelajar dan mahasiswa dalam memahami serta mengklasifikasikan gugus fungsi dalam senyawa organik. 
     
     Dalam pembelajaran kimia organik, pengenalan struktur senyawa dan pengklasifikasian gugus fungsinya seringkali menjadi tantangan, terutama bagi pemula. Oleh karena itu, aplikasi ini hadir sebagai media interaktif yang dapat membantu pengguna mengidentifikasi berbagai gugus fungsi seperti alkohol, eter, aldehid, keton, asam karboksilat, dan lain-lain secara lebih mudah dan cepat.
 
     
-    Selain itu, aplikasi ini juga dirancang untuk *membantu pengguna dalam mengetahui nama senyawa berdasarkan aturan penamaan IUPAC maupun nama trivial*. Penamaan senyawa organik merupakan bagian penting dalam komunikasi kimia, dan memahami aturan penamaan dapat meningkatkan pemahaman terhadap struktur serta reaktivitas senyawa tersebut.
+    Selain itu, aplikasi ini juga dirancang untuk *membantu pengguna dalam mengetahui nama senyawa berdasarkan aturan penamaan IUPAC maupun nama trivial. Penamaan senyawa organik merupakan bagian penting dalam komunikasi kimia, dan memahami aturan penamaan dapat meningkatkan pemahaman terhadap struktur serta reaktivitas senyawa tersebut.
 
     
-    Keunggulan dari aplikasi ini adalah adanya *fitur input atau visualisasi struktur senyawa, baik secara manual melalui antarmuka interaktif maupun melalui gambar (jika didukung). Dengan fitur ini, pengguna tidak hanya membaca teks, tetapi juga dapat melihat langsung bentuk struktur senyawa yang dimaksud, sehingga **proses identifikasi dan penamaan menjadi lebih intuitif dan efisien*.
+    Keunggulan dari aplikasi ini adalah adanya *fitur input atau visualisasi struktur senyawa, baik secara manual melalui antarmuka interaktif maupun melalui gambar (jika didukung). Dengan fitur ini, pengguna tidak hanya membaca teks, tetapi juga dapat melihat langsung bentuk struktur senyawa yang dimaksud, sehingga proses identifikasi dan penamaan menjadi lebih intuitif dan efisien.
 
     
-    Secara keseluruhan, web ini dibuat sebagai *alat bantu belajar yang praktis dan edukatif*, yang dapat diakses kapan saja dan di mana saja. Dengan memanfaatkan teknologi digital, aplikasi ini bertujuan untuk mendekatkan konsep kimia organik kepada pengguna secara visual, sistematis, dan menyenangkan.
+    Secara keseluruhan, web ini dibuat sebagai alat bantu belajar yang praktis dan edukatif, yang dapat diakses kapan saja dan di mana saja. Dengan memanfaatkan teknologi digital, aplikasi ini bertujuan untuk mendekatkan konsep kimia organik kepada pengguna secara visual, sistematis, dan menyenangkan.
 """)
 
     
@@ -154,7 +154,7 @@ def identifikasi():
         unsafe_allow_html=True
     )
     
-    st.title("🧪 Identifikasi Gugus Fungsi & Penamaan Senyawa Organik")
+    st.title("🧪 Identifikasi Gugus Fungsi & Penamaan Senyawa Hidrokarbon")
 
     # st.image("https://raw.githubusercontent.com/RIVI44/LPK-KEDUA-/main/WhatsApp%20Image%202025-07-19%20at%2013.17.34_bfbfabba.jpg", use_container_width=True)
 
