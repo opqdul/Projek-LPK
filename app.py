63ce788308a94f656c2228c7f4cd9c5c88a6bee4
RIVI44 <riviamaharani0@gmail.com>
2025-07-20 16:47:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ kamus_nama_senyawa = {
     'CH3CH2CHO': {'iupac': 'Propanal', 'trivial': 'Propionaldehida', 'golongan': 'Aldehid', 'rumus_umum': 'R-CHO'},
     'CH3(CH2)2CHO': {'iupac': 'Butanal', 'trivial': 'Butiraldehida', 'golongan': 'Aldehid', 'rumus_umum': 'R-CHO'},
     'CH3(CH2)3CHO': {'iupac': 'Pentanal', 'trivial': 'Valeraldehida', 'golongan': 'Aldehid', 'rumus_umum': 'R-CHO'},
-    'C6H5CHO': {'iupac': 'Benzena karbaldehida', 'trivial': 'Benzaldehida', 'golongan': 'Aldehid aromatik', 'rumus_umum':'Ar-CHO'},
+    'C6H5CHO': {'iupac': 'Benzena karbaldehida', 'trivial': 'Benzaldehida','gambar':'benzenakarbaldehida.png', 'golongan': 'Aldehid aromatik', 'rumus_umum':'Ar-CHO'},
     'HCOOH': {'iupac': 'Asam metanoat', 'trivial': 'Asam format','gambar':'as.metanoat.png', 'golongan': 'Asam karboksilat', 'rumus_umum': 'R-COOH'},
     'CH3COOH': {'iupac': 'Asam etanoat', 'trivial': 'Asam asetat','gambar':'as.etanoat.png', 'golongan': 'Asam karboksilat', 'rumus_umum': 'R-COOH'},
     'CH3CH2COOH': {'iupac': 'Asam propanoat', 'trivial': 'Asam propionat','gambar':'as.propanoat.png', 'golongan': 'Asam karboksilat', 'rumus_umum': 'R-COOH'},
