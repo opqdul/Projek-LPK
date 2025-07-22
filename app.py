b7ba34138d88351e354b5bc2b649f3d853e631e2
RIVI44 <riviamaharani0@gmail.com>
2025-07-22 17:49:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ kamus_nama_senyawa = {
     'CH3CH2CH2CH2CH2CH2CH2CH2CH2CH3': {'iupac': 'Dekana', 'trivial': '-',"golongan":"Alkana","rumus_umum":"CnH2n+2"},
     'CH2CH2': {'iupac': 'Etena', 'trivial': 'Etilena',"golongan":"Alkena","rumus_umum":"CnH2n"},
     'CH2CHCH3': {'iupac': 'Propena', 'trivial': 'Propilena','gambar':'propena.png',"golongan":"Alkena","rumus_umum":"CnH2n"},
-    'CH2CHCH2CH3': {'iupac': 'Butena', 'trivial': '-',"golongan":"Alkena","rumus_umum":"CnH2n"},
+    'CH2CHCH2CH3': {'iupac': 'Butena', 'trivial': '-','gambar':'butenas.png',"golongan":"Alkena","rumus_umum":"CnH2n"},
     'CH2CHCH2CH2CH3': {'iupac': 'Pentena', 'trivial': '-',"golongan":"Alkena","rumus_umum":"CnH2n"},
     'CH2CHCH2CH2CH2CH3': {'iupac': 'Heksena', 'trivial': '-',"golongan":"Alkena","rumus_umum":"CnH2n"},
     'CH2CHCH2CH2CH2CH2CH3': {'iupac': 'Heptena', 'trivial': '-',"golongan":"Alkena","rumus_umum":"CnH2n"},
