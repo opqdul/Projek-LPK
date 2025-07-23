559fae54ade15e365f273b035097e159a53a33dc
RIVI44 <riviamaharani0@gmail.com>
2025-07-23 14:20:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def tentang():
         f"""
         <style>
             .blurimg {{
-                background-image: url("https://raw.githubusercontent.com/RIVI44/-PROJEK_LPK_/main/wakpaper2.jpg");
+                background-image: url("https://raw.githubusercontent.com/RIVI44/-PROJEK_LPK_/main/bg2.jpgg");
                 background-size: cover;
                 position: fixed;
                 inset: 0;
